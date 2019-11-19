1e41bbcf522837a302223ce9c9bd3ee45f0f7855
Eric Cheatham <ericjcheatham@gmail.com>
2019-11-18 22:46:43 -0600

    Connect amqp source

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,36 @@
-import akka.Done
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.alpakka.amqp.scaladsl.AmqpSink
-import akka.stream.alpakka.amqp.{AmqpLocalConnectionProvider, AmqpWriteSettings, QueueDeclaration}
+import akka.stream.alpakka.amqp.scaladsl.AmqpSource
+import akka.stream.alpakka.amqp.{AmqpLocalConnectionProvider, NamedQueueSourceSettings, QueueDeclaration, ReadResult}
 import akka.stream.scaladsl.{Sink, Source}
-import akka.util.ByteString
-
-import scala.concurrent.Future
-
+import org.slf4j.LoggerFactory
 
 object Main extends App {
   implicit val system: ActorSystem = ActorSystem("SimpleQueueExample")
   implicit val mat: ActorMaterializer = ActorMaterializer()
 
+  val logger = LoggerFactory.getLogger(getClass)
+
   // Create the queue to publish to
-  val queueName = "amqp-conn-it-test-simple-queue-" + System.currentTimeMillis()
+  val bufferSize = 10
+  val queueName = "amqp-conn-it-test-simple-queue-example"
   val queueDeclaration = QueueDeclaration(queueName)
 
   val connectionProvider = AmqpLocalConnectionProvider
 
-  val queueSink: Sink[ByteString, Future[Done]] =
-    AmqpSink.simple(
-      AmqpWriteSettings(AmqpLocalConnectionProvider)
-        .withRoutingKey(queueName)
+  val queueSource: Source[ReadResult, NotUsed] =
+    AmqpSource.atMostOnceSource(
+      NamedQueueSourceSettings(connectionProvider, queueName)
         .withDeclaration(queueDeclaration)
+        .withAckRequired(true),
+      bufferSize = bufferSize
     )
 
-  val input = Vector("one", "two", "three", "four", "five")
-
-  val writing: Future[Done] =
-    Source(input)
-      .map(s => ByteString(s))
-      .runWith(queueSink)
-
+  val result =
+    queueSource
+      .take(bufferSize)
+      .map({ x ⇒ x.bytes.utf8String })
+      .alsoTo(Sink.foreach { x ⇒ println(s"Received message: $x") })
+      .runWith(Sink.seq)
 }
---
+++ src/main/scala/models/CustomerCreated.scala
@@ -0,0 +1,3 @@
+package models
+
+case class CustomerCreated(id: String, name: String, email: String)
